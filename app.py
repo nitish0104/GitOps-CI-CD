27e5b5ecf81529943d2fa5f826e86b511c664e5a
Nitish0104 <nitishdalvi1@gmail.com>
2024-01-28 19:22:00 +0530

    Todo app with docker file

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Mock todo list for demonstration
+todo_list = [
+    {"id": 1, "task": "Buy groceries"},
+    {"id": 2, "task": "Finish coding"},
+    {"id": 3, "task": "Go for a run"},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', todo_list=todo_list)
+
+@app.route('/add', methods=['POST'])
+def add():
+    new_task = request.form.get('task')
+    if new_task:
+        todo_list.append({"id": len(todo_list) + 1, "task": new_task})
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    global todo_list
+    todo_list = [task for task in todo_list if task['id'] != task_id]
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
